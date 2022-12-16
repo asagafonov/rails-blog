b52b09c74852a6e000c7dcc2da301fe6855da334
asagafonov <a.s.agafonov@yandex.ru>
2022-12-16 18:39:45 +0300

    fix linter issues

---
+++ config/initializers/sentry.rb
@@ -0,0 +1,13 @@
+Sentry.init do |config|
+  config.dsn = 'https://a7bddd0ae02b46569c7a814e7ad57afb@o4504339358482432.ingest.sentry.io/4504339359662080'
+  config.breadcrumbs_logger = %i[active_support_logger http_logger]
+
+  # Set traces_sample_rate to 1.0 to capture 100%
+  # of transactions for performance monitoring.
+  # We recommend adjusting this value in production.
+  config.traces_sample_rate = 1.0
+  # or
+  config.traces_sampler = lambda do |_context|
+    true
+  end
+end
