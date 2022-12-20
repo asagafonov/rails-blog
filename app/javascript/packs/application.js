548bf65138e5a12d0fd28215168fdee9e5d2764d
asagafonov <agafonov@buroburo.ru>
2022-12-20 12:35:49 +0300

    add bootstrap

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -15,4 +15,8 @@
 // const images = require.context('../images', true)
 // const imagePath = (name) => images(name, true)
 
-console.log('Hello World from Webpacker')
+import "@hotwired/turbo-rails"  
+// import "./controllers"  
+import * as bootstrap from "bootstrap"  
+
+console.log('js on')
