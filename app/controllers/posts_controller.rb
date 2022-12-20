11cb7ae6709e4115954d370783841bc1278d7dff
asagafonov <agafonov@buroburo.ru>
2022-12-20 18:02:24 +0300

    upd controllers

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,58 @@
+class PostsController < ApplicationController
+  def index
+    @posts = Post.by_creation_date_desc
+  end
+
+  def show
+    @post = Post.find(params[:id])
+  end
+
+  def new
+    @post = Post.new
+  end
+  
+  def create
+    @post = Post.new(post_params)
+    category = Category.find(post_params[:category_id])
+    user = User.find(post_params[:user_id])
+    @post.category = category
+    @post.user = user
+
+    if @post.save
+      redirect_to @post, notice: 'Post created'
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def edit
+    @post = Post.find(params[:id])
+  end
+
+  def update
+    @post = Post.new(post_params)
+    category = Category.find(post_params[:category_id])
+    user = User.find(post_params[:user_id])
+    @post.category = category
+    @post.user = user
+
+    if @post.update(post_params)
+      redirect_to @post, notice: 'Post updated'
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @post = Post.find(params[:id])
+
+    @post.destroy
+    redirect_to posts_url, notice: 'Post deleted'
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:category_id, :creator, :description, :title, :user_id)
+  end
+end
