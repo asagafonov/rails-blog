9f112a5234e4eba4d67b7c599e961d61da50ad4a
asagafonov <agafonov@buroburo.ru>
2022-12-20 12:36:03 +0300

    add devise login

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,12 @@
 Rails.application.routes.draw do
+  get 'home/index'
+  root 'home#index'
+  devise_for :users, path: 'auth', path_names: { sign_in: 'login', sign_out: 'logout', password: 'secret', confirmation: 'verification', unlock: 'unblock', registration: 'register', sign_up: 'cmon_let_me_in' }
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
+  devise_scope :user do
+    get 'sign_in', to: 'devise/sessions#new'
+  end
   # Defines the root path route ("/")
   # root "articles#index"
 end
