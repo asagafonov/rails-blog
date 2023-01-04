d3a5f67e9d2d090e1b8ab6bbf1bfd35a863eb8ca
asagafonov <agafonov@buroburo.ru>
2023-01-04 11:28:39 +0300

    remove locale scope

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,16 +1,16 @@
 # frozen_string_literal: true
 
 class ApplicationController < ActionController::Base
-  before_action :set_locale
+  # before_action :set_locale
 
-  private
+  # private
 
-  def set_locale
-    locale = params[:locale] || I18n.default_locale
-    I18n.locale = locale.to_sym
-  end
+  # def set_locale
+  #   locale = params[:locale] || I18n.default_locale
+  #   I18n.locale = locale.to_sym
+  # end
 
-  def default_url_options
-    { locale: I18n.locale }
-  end
+  # def default_url_options
+  #   { locale: I18n.locale }
+  # end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,25 +1,23 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  scope '(:locale)', locale: /ru|en/ do
-    root 'posts#index'
-    devise_for :users, path_names: { sign_in: 'sign_in', sign_out: 'sign_out', sign_up: 'sign_up' }
+  root 'posts#index'
+  devise_for :users, path_names: { sign_in: 'sign_in', sign_out: 'sign_out', sign_up: 'sign_up' }
 
-    devise_scope :user do
-      get 'sign_in', to: 'devise/sessions#new'
-      get 'users/sign_out', to: 'devise/sessions#destroy'
-      get 'sign_up', to: 'devise/registrations#new'
+  devise_scope :user do
+    get 'sign_in', to: 'devise/sessions#new'
+    get 'users/sign_out', to: 'devise/sessions#destroy'
+    get 'sign_up', to: 'devise/registrations#new'
+  end
+
+  resources :posts do
+    scope module: :posts, shallow: true do
+      resources :comments, only: %i[show edit update destroy]
     end
 
-    resources :posts do
-      scope module: :posts, shallow: true do
-        resources :comments, only: %i[show edit update destroy]
-      end
-
-      scope module: :posts do
-        resources :comments, only: %i[index new create]
-        resources :likes, only: %i[create destroy]
-      end
+    scope module: :posts do
+      resources :comments, only: %i[index new create]
+      resources :likes, only: %i[create destroy]
     end
   end
 end
