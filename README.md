ab9a940d757b8783916146df06910851641ff252
asagafonov <a.s.agafonov@yandex.ru>
2023-01-09 12:31:29 +0300

    add new deployment link

--- README.md
+++ README.md
@@ -4,4 +4,4 @@
 [![linter & tests](https://github.com/asagafonov/rails-project-64/actions/workflows/linter-and-tests.yml/badge.svg?branch=main)](https://github.com/asagafonov/rails-project-64/actions/workflows/linter-and-tests.yml)
 
 ### Deployed at
-rails-project-64-production-82fb.up.railway.app
+rails-project-64-production-6e5e.up.railway.app
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -34,6 +34,7 @@ Rails.application.configure do
 
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
   # config.asset_host = "http://assets.example.com"
+  config.hosts << 'rails-project-64-production-6e5e.up.railway.app'
 
   # Specifies the header that your server uses for sending files.
   # config.action_dispatch.x_sendfile_header = "X-Sendfile" # for Apache
