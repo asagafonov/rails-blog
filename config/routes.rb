c6f209db08cb6c087a58e5f8b1dbfd46e51e91e9
asagafonov <a.s.agafonov@yandex.ru>
2023-01-10 17:15:02 +0300

    upd devise views & forms

--- config/routes.rb
+++ config/routes.rb
@@ -2,13 +2,7 @@
 
 Rails.application.routes.draw do
   root 'posts#index'
-  devise_for :users, path_names: { sign_in: 'sign_in', sign_out: 'sign_out', sign_up: 'sign_up' }
-
-  devise_scope :user do
-    get 'sign_in', to: 'devise/sessions#new'
-    get 'users/sign_out', to: 'devise/sessions#destroy'
-    get 'sign_up', to: 'devise/registrations#new'
-  end
+  devise_for :users, controller: { sessions: 'users/sessions', registrations: 'users/registrations' }
 
   resources :posts do
     scope module: :posts, shallow: true do
