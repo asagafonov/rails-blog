7d9ef11517d35ab1e15bb093bbdcc566ace332d3
asagafonov <a.s.agafonov@yandex.ru>
2023-01-18 14:21:46 +0300

    remove unused param

--- app/controllers/posts/comments_controller.rb
+++ app/controllers/posts/comments_controller.rb
@@ -41,7 +41,7 @@ module Posts
     end
 
     def comment_params
-      params.require(:post_comment).permit(:content, :post_id)
+      params.require(:post_comment).permit(:content)
     end
   end
 end
