20319c89bdc37e1a2cce47ec2604114b6cdc3f84
asagafonov <agafonov@buroburo.ru>
2023-01-18 10:42:26 +0300

    add dependent key to category and user models

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,9 +3,9 @@
 class User < ApplicationRecord
   devise :database_authenticatable, :registerable, :rememberable, :validatable
 
-  has_many :posts
-  has_many :comments, class_name: 'PostComment'
-  has_many :likes, dependent: :destroy, class_name: 'PostLike'
+  has_many :posts, dependent: :destroy
+  has_many :comments, class_name: 'PostComment', dependent: :destroy
+  has_many :likes, dependent: :destroy, class_name: 'PostLike', dependent: :destroy
 
   validates :email, :password, presence: true
 end
