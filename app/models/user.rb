79c93c23a96651c9e321ad43007d973bc820fe27
asagafonov <agafonov@buroburo.ru>
2023-01-18 12:15:39 +0300

    remove duplicate key

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,7 @@ class User < ApplicationRecord
 
   has_many :posts, dependent: :destroy
   has_many :comments, class_name: 'PostComment', dependent: :destroy
-  has_many :likes, dependent: :destroy, class_name: 'PostLike', dependent: :destroy
+  has_many :likes, class_name: 'PostLike', dependent: :destroy
 
   validates :email, :password, presence: true
 end
